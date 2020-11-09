343970969552bceba4f8e352ba80e87046733ccf
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-11-09 15:29:58 -0300

    Listando Paciente por paginação

--- src/main/java/com/vicente/apiprontuarionline/services/PacienteService.java
+++ src/main/java/com/vicente/apiprontuarionline/services/PacienteService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.vicente.apiprontuarionline.model.Paciente;
@@ -44,5 +47,11 @@ public class PacienteService {
 
 	public List<Paciente> findAllPaciente() {
 		return pacienteRepository.findAll();
+	}
+
+	// PAGINAÇÃO DE PACIENTES NO BANCO DE DADOS
+	public Page<Paciente> findPagePaciente(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return pacienteRepository.findAll(pageRequest);
 	}
 }
